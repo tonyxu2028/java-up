17e2091e91f90caf663b4af6bc5cf3a13432f31c
tonyxu2028 <liwei2028@163.com>
2023-09-26 10:25:49 +0800

    111

--- jus.crypto.use/src/main/java/co/ivi/jus/use/UseCase.java
+++ jus.crypto.use/src/main/java/co/ivi/jus/use/UseCase.java
@@ -11,15 +11,6 @@ import co.ivi.jus.crypto.Returned;
 public class UseCase {
     public static void main(String[] args) {
         Returned<Digest> rt = Digest.of("SHA-256");
-        switch (rt) {
-            case Returned.ReturnValue rv -> {
-                Digest d = (Digest) rv.returnValue();
-                d.digest("Hello, world!".getBytes());
-            }
-            case Returned.ErrorCode ec ->
-                    System.getLogger("co.ivi.jus.stack.union")
-                            .log(System.Logger.Level.INFO,
-                                    "Failed to get instance of SHA-256");
-        }
+//
     }
 }
