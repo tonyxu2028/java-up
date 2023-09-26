e1e8b03d90d2df7ccd3de1971647e1d491229a2a
tonyxu2028 <liwei2028@163.com>
2023-09-26 15:25:15 +0800

    加入注释

--- src/main/java/co/ivi/jus/record/immute/Circle.java
+++ src/main/java/co/ivi/jus/record/immute/Circle.java
@@ -3,9 +3,15 @@
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  */
 
+/**
+ * 这是一种只读设计，通过这种设计可以有效地避免对象的状态被修改。
+ * 从而导致多线性环境下的数据竞争安全。
+ */
 package co.ivi.jus.record.immute;
 
 public final class Circle implements Shape {
+
+    // 通过 final 修饰符来保证对象的状态不会被修改。
     public final double radius;
 
     public Circle(double radius) {
--- src/main/java/co/ivi/jus/record/modern/Circle.java
+++ src/main/java/co/ivi/jus/record/modern/Circle.java
@@ -1,8 +1,10 @@
 /*
  * Copyright (c) 2021, Xuelei Fan. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Circle档案类,表示不可修改的透明载体。
+ * Circle(double radius)等同于Circle { Circle(double radius) }的构造方法。
  */
-
 package co.ivi.jus.record.modern;
 
 public record Circle(double radius) implements Shape {
