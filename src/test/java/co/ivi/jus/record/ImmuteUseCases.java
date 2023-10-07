43035379bb9673f343beb208b7a651b56275ddd1
tonyxu2028 <liwei2028@163.com>
2023-10-07 14:03:25 +0800

    加入注释

--- src/test/java/co/ivi/jus/record/ImmuteUseCases.java
+++ src/test/java/co/ivi/jus/record/ImmuteUseCases.java
@@ -1,8 +1,9 @@
-/*
+/**
  * Copyright (c) 2021, Xuelei Fan. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * 判断两个相同半径的圆是否相等，以及打印圆的信息。
  */
-
 package co.ivi.jus.record;
 
 import co.ivi.jus.record.immute.Circle;
@@ -13,6 +14,7 @@ public class ImmuteUseCases {
         Circle c2 = new Circle(10.0);
 
         System.out.println("Equals? " + c1.equals(c2));
-        System.out.println("It is a " + c1);
+        System.out.println("It is a c1 " + c1);
+        System.out.println("It is a c2 " + c2);
     }
 }
